8c7a09fb35b5494654cf55d16205e928100f4c0b
Jeff Cheung <jeff.h.cheung@gmail.com>
2011-02-09 15:02:03 -0800

    Added comments for ClassTableVisitor

--- src/minijava/typechecker/implementation/ClassTableVisitor.java
+++ src/minijava/typechecker/implementation/ClassTableVisitor.java
@@ -14,6 +14,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
     FunTable<Info> t = FunTable.theEmpty();
     for(int i = n.size() - 1; i >= 0; --i)
     {
+      // Iterate all nodes and find symbols
       t = t.merge(n.elementAt(i).accept(this));
     }
     
@@ -23,6 +24,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Program n)
   {
+    // Find symbols in MainClass and other classes
     return n.mainClass.accept(this)
                       .merge(n.classes.accept(this));
   }
@@ -32,17 +34,20 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   {
     FunTable<Info> t = FunTable.theEmpty();
     
-    // Add main() to ClassInfo
+    // Create main MethodInfo declaration
     MethodInfo m  = new MethodInfo();
     m.formals     = t;
     m.locals      = t;
     m.formalsList = new ArrayList<VarInfo>();
     
+    // Create main ClassInfo declaration
     ClassInfo c = new ClassInfo();
     c.name      = n.className;
     c.fields    = t;
     c.methods   = t.insert("main", m);
     
+    // Add to symbol table
+    // Find symbols in statements and combine with the symbol table
     return t.insert(n.className, c)
             .merge(n.statement.accept(this));
   }
@@ -50,6 +55,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(ClassDecl n)
   {
+    // Create ClassInfo declaration
     ClassInfo c   = new ClassInfo();
     c.name        = n.name;
     c.superClass  = n.superName;
@@ -63,6 +69,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(VarDecl n)
   {
+    // Create VarInfo declaration
     VarInfo v = new VarInfo();
     v.kind    = n.kind;
     v.type    = n.type;
@@ -74,12 +81,14 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(MethodDecl n)
   {
+    // Create MethodInfo declaration
     MethodInfo m  = new MethodInfo();
     m.formals     = n.formals.accept(this);
     m.formalsList = new ArrayList<VarInfo>();
     m.locals      = n.vars.accept(this);
     m.returnType  = n.returnType;
     
+    // Create VarInfo declarations for each argument
     for(int i = 0; i < n.formals.size(); ++i)
     {
       VarDecl d   = n.formals.elementAt(i);
@@ -89,6 +98,8 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
       m.formalsList.add(v);
     }
     
+    // Add to symbol table
+    // Find symbols in statements and return expressions and combine
     FunTable<Info> t = FunTable.theEmpty();
     return t.insert(n.name, m)
             .merge( n.statements.accept(this)
@@ -110,12 +121,14 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Block n)
   {
+    // Find symbols in statements
     return n.statements.accept(this);
   }
 
   @Override
   public FunTable<Info> visit(If n)
   {
+    // Find symbols in condition, then, and else statements
     return n.tst.accept(this)
                 .merge(n.thn.accept(this)
                             .merge(n.els.accept(this)));
@@ -124,6 +137,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(While n)
   {
+    // Find symbols in condition and body statements
     return n.tst.accept(this)
                 .merge(n.body.accept(this));
   }
@@ -131,18 +145,21 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Print n)
   {
+    // Find symbols in print statement
     return n.exp.accept(this);
   }
 
   @Override
   public FunTable<Info> visit(Assign n)
   {
+    // Find symbols in value
     return n.value.accept(this);
   }
 
   @Override
   public FunTable<Info> visit(ArrayAssign n)
   {
+    // Find symbols in index and value
     return n.index.accept(this)
                   .merge(n.value.accept(this));
   }
@@ -150,6 +167,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(And n)
   {
+    // Find symbols in left and right expressions
     return  n.e1.accept(this)
                 .merge(n.e2.accept(this));
   }
@@ -157,6 +175,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(LessThan n)
   {
+    // Find symbols in left and right expressions
     return  n.e1.accept(this)
                 .merge(n.e2.accept(this));
   }
@@ -164,6 +183,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Plus n)
   {
+    // Find symbols in left and right expressions
     return  n.e1.accept(this)
                 .merge(n.e2.accept(this));
   }
@@ -171,6 +191,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Minus n)
   {
+    // Find symbols in left and right expressions
     return  n.e1.accept(this)
                 .merge(n.e2.accept(this));
   }
@@ -178,6 +199,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Times n)
   {
+    // Find symbols in left and right expressions
     return  n.e1.accept(this)
                 .merge(n.e2.accept(this));
   }
@@ -185,6 +207,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(ArrayLookup n)
   {
+    // Find symbols in index and array expressions
     return n.index.accept(this)
                   .merge(n.array.accept(this));
   }
@@ -192,12 +215,14 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(ArrayLength n)
   {
+    // Find symbols in array expression
     return n.array.accept(this);
   }
 
   @Override
   public FunTable<Info> visit(Call n)
   {
+    // Find symbols in receiver and rands expressions and combine
     return  n.receiver.accept(this)
                       .merge(n.rands.accept(this));
   }
@@ -217,6 +242,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(NewArray n)
   {
+    // Find symbols in size expression
     return n.size.accept(this);
   }
 
@@ -226,6 +252,7 @@ public class ClassTableVisitor implements Visitor<FunTable<Info>>
   @Override
   public FunTable<Info> visit(Not n)
   {
+    // Find symbols in boolean expression
     return n.e.accept(this);
   }
 }
